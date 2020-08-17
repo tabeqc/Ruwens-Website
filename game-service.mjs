de8a3ca5b4258596be747898002904c8bb3439a8
Ruwen Frick <63873305+tabeqc@users.noreply.github.com>
2020-08-17 21:35:20 +0200

    Update game-service.mjs

--- game-service.mjs
+++ game-service.mjs
@@ -3,20 +3,23 @@
 import * as waiting from "./script.mjs";
 
 export async function getServerRanking(responseReceivedCallbackFn) {
-    waiting.setWaitingTrue();
-    await sleep(1000);
+    waiting.setWaitingRankingTrue();
+    await sleep(350);
     let url = 'https://us-central1-schere-stein-papier-ee0C9.cloudfunctions.net/widgets/ranking';
     const response = await fetch(url);
-    const data = await response.json().then(responseReceivedCallbackFn());
+    const data = await response.json();
+    responseReceivedCallbackFn();
     return Object.entries(data);
 }
 
-export async function getServerGame (yourPick, playerName) {
+export async function getServerGame (yourPick, playerName, responseReceivedCallbackFn) {
+    waiting.setWaitingGameTrue();
     let url = `https://us-central1-schere-stein-papier-ee0C9.cloudfunctions.net/widgets/play?playerName=${playerName}&playerHand=${yourPick}`;
-    const response = await fetch(url).catch(() => console.log("Error!"));
+    const response = await fetch(url);
     const data = await response.json();
     const choice = await data.choice;
     let result = await data.win;
+    responseReceivedCallbackFn();
     return {enemyPick: choice,
             result: result};
 }
